fac283c49d4609eb5152bb41f834d22c4bebbae1
agunawan <agunawan@ascentis.com>
2019-08-20 12:14:13 -0700

    Add Swagger and HATEOAS basic implementations
    - Add Swagger to gradle
    - Configure Swagger
    - Use Swagger annotations in the Greeting Controller
    - Implements HATEOAS in Greeting class

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -1,16 +1,30 @@
 package hello;
 
-public class Greeting {
+import org.springframework.hateoas.ResourceSupport;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Greeting extends ResourceSupport {
 
     private final long id;
     private final String content;
 
-    public Greeting(long id, String content) {
+    @JsonCreator
+    public Greeting(@JsonProperty("content") String content) {
+        this.id = 1;
+        this.content = content;
+    }
+
+    @JsonCreator
+    public Greeting(
+        @JsonProperty("id") long id,
+        @JsonProperty("content") String content) {
         this.id = id;
         this.content = content;
     }
 
-    public long getId() {
+    public long getGreetingId() {
         return id;
     }
 
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,19 +1,41 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Api(value = "GreetingController", description = "REST Apis for greeting implements HATEOAS")
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    @ApiOperation(value = "Get the greeting message", response = Greeting.class, tags = "greeting")
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Success|OK"),
+        @ApiResponse(code = 401, message = "not authorized!"),
+        @ApiResponse(code = 403, message = "forbidden!!!"),
+        @ApiResponse(code = 404, message = "not found!!!") })
+    @RequestMapping(method = RequestMethod.GET, value = "/greeting")
+    public HttpEntity<Greeting> greeting(
+        @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+
+        Greeting greeting = new Greeting(counter.incrementAndGet(), String.format(template, name));
+        greeting.add(linkTo(methodOn(GreetingController.class).greeting(name)).withSelfRel());
+
+        return new ResponseEntity<>(greeting, HttpStatus.OK);
     }
 }
